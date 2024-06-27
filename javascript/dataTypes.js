d2286c26f0ea4a8fc3d8a179da5885ca4e1ae3b1
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-06-27 16:18:39 +0530

    datatypesIn js

---
+++ javascript/dataTypes.js
@@ -0,0 +1,17 @@
+// THERE ARE BASICALLY 7 TYPES OF DATATYPES ARE USED IN JAVASCRIPT
+
+let name="ganesh"
+let age=29
+let isLoggedIn=true/false
+let val=null
+
+let id=undefined
+
+let char=Symbol(age)
+// console.log(char)
+ 
+BigInt
+
+console.log(typeof char)
+console.log(typeof undefined)
+console.log(typeof null)
