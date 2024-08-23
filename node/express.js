520cdfe33c315a918e34cf9761c3e5af90ac7004
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-08-23 22:32:36 +0530

    changes

--- node/express.js
+++ node/express.js
@@ -1,21 +1,22 @@
 // console.log("Express servers")
 
 // const express=require("express")
-const express=require("express")
-const app=express()
+const express = require("express")
+const app = express()
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     // console.log(res.hello)
     // res.send("<h1>Hello Backend</h1>");
-    res.json({"message":"<h1>Hello Backend</h1>"})
+    // res.json({ "message": "<h1>Hello Backend</h1>" })
+    res.send(req.query)
 })
-app.get('/home',(req,res)=>{
+app.get('/home', (req, res) => {
     res.send("<h1>I Will Dominate Backend</h1>")
 })
-app.get('/login',(req,res)=>{
+app.get('/login', (req, res) => {
     res.send("<h1>Login Page Here...</h1>")
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("server runing")
 })
