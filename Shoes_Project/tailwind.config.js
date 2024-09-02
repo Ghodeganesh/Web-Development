e9cb54aabb72b20350c5a12c22c9ad8562f778c5
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-09-03 01:22:28 +0530

    the_shoe_company

--- Shoes_Project/tailwind.config.js
+++ Shoes_Project/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'media560':'560px'
+     },
+       
+    },
   },
   plugins: [],
 }
